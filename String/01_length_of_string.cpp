448a279dc87a96a0a4e6722212a22f3a15c6e07a
Rahul sharma <rahulaki.09025@gmail.com>
2022-11-09 14:59:34 +0530

    added string operations

---
+++ String/01_length_of_string.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char name[]="Rahul Sharma";
+    int i;
+    for(i=0;name[i] !='\0';i++)
+    {
+    }
+    cout<<"lenght of string is " <<i;
+    return 0;
+}
